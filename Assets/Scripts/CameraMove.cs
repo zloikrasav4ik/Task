63b8f867e452ed46e7ee6fabc949eb30fda06f6c
Roma <roma_lesan@rambler.ru>
2021-01-25 17:40:53 +0200

    project

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(0f,60f,player.transform.position.z);
+    }
+}
